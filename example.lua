305d062091f752d5a3e28e7a101d348b15487bcc
PeaPattern <72760536+PeaPattern@users.noreply.github.com>
2024-02-09 22:59:14 -0500

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,9 +1,9 @@
 local Connection, Remote = loadstring(game:HttpGet("https://raw.githubusercontent.com/PeaPattern/legacy-hook/main/main.lua"))()
 local Players = game:GetService("Players")
 
-Connection.Event:Connect(function(Message)
+Connection.OnInvoke = function(Message)
     for _ = 1, 5 do
         Players:Chat("filter reset with chat function")
     end
     Remote:FireServer(string.format("[hook]: %s", Message), "All")
-end)
+end
