04e5e97b353ac37fa3b0d62bbe11367286417e80
PeaPattern <72760536+PeaPattern@users.noreply.github.com>
2024-02-09 23:02:47 -0500

    Update example.lua

--- example.lua
+++ example.lua
@@ -3,6 +3,6 @@ local Players = game:GetService("Players")
 local Chat = game:GetService("Chat")
 
 Connection.OnInvoke = function(Message)
-    Chat:FilterStringForBroadcast(math.random(1000000000, 9999999999) .. " filter reset with chat function"))
+    Chat:FilterStringForBroadcast(math.random(1000000000, 9999999999) .. " filter reset with chat function")
     Remote:FireServer(string.format("%s", Message), "All")
 end
