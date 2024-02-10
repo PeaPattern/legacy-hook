4b67979e07ee4b3039ce48008e3fa14dd586188e
PeaPattern <72760536+PeaPattern@users.noreply.github.com>
2024-02-09 23:03:31 -0500

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,8 +1,9 @@
 local Connection, Remote = loadstring(game:HttpGet("https://raw.githubusercontent.com/PeaPattern/legacy-hook/main/main.lua"))()
 local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
 local Chat = game:GetService("Chat")
 
 Connection.OnInvoke = function(Message)
-    Chat:FilterStringForBroadcast(math.random(1000000000, 9999999999) .. " filter reset with chat function")
+    Chat:FilterStringForBroadcast(math.random(1000000000, 9999999999) .. " filter reset with chat function", LocalPlayer)
     Remote:FireServer(string.format("%s", Message), "All")
 end
